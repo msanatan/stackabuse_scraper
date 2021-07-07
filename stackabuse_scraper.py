b275db76ceca397ab6e5655ec6a8ed2e2df51337
Marcus Sanatan <msanatan@gmail.com>
2021-07-07 10:33:09 -0400

    Update scraper to get articles in new format
    We need to look at each article's page to get more data

--- stackabuse_scraper.py
+++ stackabuse_scraper.py
@@ -12,50 +12,61 @@ from argparse import ArgumentParser
 
 BASE_URL = 'https://stackabuse.com'
 
-def parse_posts(author_url):
+
+def parse_posts(author_url, defaut_editor):
     '''Recursively retrieves all the posts of an blog write in stack abuse'''
     logging.info('Scraping {}'.format(author_url))
     posts = []
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'}
     response = requests.get(author_url, headers=headers)
     if response is not None and response.status_code == 200:
         html = BeautifulSoup(response.content, 'html.parser')
-        for article in html.find_all('article'):
-            title_tag = article.find('h2', {'class': 'post-title'}).find('a')
-            title = title_tag.text
-            link = BASE_URL + title_tag['href']
-            meta = article.find('div', {'class': 'post-meta'})
-            date_text = meta.find('span', {'class': 'date'}).text
-            date = datetime.datetime.strptime(date_text, '%B %d, %Y')
+        for article in html.find_all('div', {'class': 'p-6'}):
+            article_details = article.find(
+                'a', {'class': 'block hover:no-underline'})
+            title = article_details.find('h3').text
+            link = BASE_URL + article_details['href']
+            meta = article.find('div', {'class': 'mt-6 flex items-center'})
+            date_text = meta.find('time')['datetime']
+            author_data = meta.find('a', {'class': 'hover:underline'})
+            author = author_data.text
+
+            # date = datetime.datetime.strptime(date_text, '%B %d, %Y')
             post = {
                 'title': title,
                 'link': link,
-                'date': datetime.date.strftime(date, "%Y-%m-%d"),
+                'date': date_text,
+                'author': author,
+                'editor': defaut_editor
             }
+
             posts.append(post)
         logging.info('{} posts found on page'.format(len(posts)))
-        # Stack Abuse paginates every 5 posts, this collects the older ones
-        pagination = html.find('nav', {'class': 'pagination'}).find('a', {'class': 'older-posts'})
+
+        # Stack Abuse paginates every 9 posts, this collects the older ones
+        pagination = html.find(
+            'a', {'class': 'border-t-2 border-transparent pt-4 pl-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300'})
         if pagination is not None:
             logging.info('Retrieving older posts')
-            return posts + parse_posts(BASE_URL + pagination['href'])
+            return posts + parse_posts(BASE_URL + pagination['href'], defaut_editor)
         return posts
     else:
         logging.error('Could not get a response for the link')
         return []
 
 
-def get_posts_json(filename, author_url):
+def get_posts_json(filename, author_url, defaut_editor):
     '''Dumps JSON for stack abuse articles'''
-    posts = parse_posts(author_url)
+    posts = parse_posts(author_url, defaut_editor)
     logging.info('Retrieved {} posts'.format(len(posts)))
     with open(filename, 'w') as json_file:
         json.dump(posts, json_file, indent=4)
 
 
-def get_posts_csv(filename, author_url):
+def get_posts_csv(filename, author_url, defaut_editor):
     '''Saves CSV file for stack abuse articles'''
-    posts = parse_posts(author_url)
+    posts = parse_posts(author_url, defaut_editor)
     logging.info('Retrieved {} posts'.format(len(posts)))
     headers = ['Title', 'Link', 'Date']
     with open(filename, 'w') as csv_file:
@@ -65,9 +76,9 @@ def get_posts_csv(filename, author_url):
             csv_writer.writerow([post['title'], post['link'], post['date']])
 
 
-def get_posts_markdown(author_url):
+def get_posts_markdown(author_url, defaut_editor):
     '''Saves posts as markdown files to work in Hexo'''
-    posts = parse_posts(author_url)
+    posts = parse_posts(author_url, defaut_editor)
     logging.info('Retrieved {} posts'.format(len(posts)))
     pathlib.Path('articles').mkdir(exist_ok=True)
     for post in posts:
@@ -77,8 +88,9 @@ def get_posts_markdown(author_url):
                 '---\n',
                 'title: "{}"\n'.format(post['title']),
                 'date: {}\n'.format(post['date']),
-                'categories: [other]\n',
                 'link: {}\n'.format(post['link']),
+                'author: {}\n'.format(post['author']),
+                'editor: {}\n'.format(post['editor']),
                 '---\n',
             ])
 
@@ -88,6 +100,9 @@ def main():
     parser = ArgumentParser(description='Web scraper for Stack Abuse writers')
     parser.add_argument('-a', '--author', dest='author',
                         help='Writer whose articles you want', required=True)
+
+    parser.add_argument('-e', '--editor', dest='editor',
+                        help='Editor for that author', required=True)
 
     group = parser.add_mutually_exclusive_group()
     group.add_argument('--csv', action='store_true',
@@ -109,16 +124,16 @@ def main():
     else:
         log_level = logging.INFO
 
-    logging.basicConfig(filename='stackabuse_scraper.log',level=log_level)
+    logging.basicConfig(filename='stackabuse_scraper.log', level=log_level)
 
     author_url = '{}/author/{}/'.format(BASE_URL, args.author)
     # Determine output format
     if args.csv:
-        get_posts_csv('stackabuse_articles.csv', author_url)
+        get_posts_csv('stackabuse_articles.csv', author_url, args.editor)
     elif args.json:
-        get_posts_json('stackabuse_articles.json', author_url)
+        get_posts_json('stackabuse_articles.json', author_url, args.editor)
     elif args.markdown:
-        get_posts_markdown(author_url)
+        get_posts_markdown(author_url, args.editor)
     else:
         print(json.dumps(parse_posts(author_url)))
 
